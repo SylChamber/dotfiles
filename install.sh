7ddfc71d8b400db9ef7cef303295bf9fde90b38d
Sylvain Chamberland <Sylvain.Chamberland@cspq.gouv.qc.ca>
2020-06-15 14:45:38 -0400

    correction des chemins dans install.sh pour chemins relatifs au script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # copie des alias
-cp $HOME/dotfiles/.bash_aliases $HOME/
+cp ./.bash_aliases $HOME/
 
 # copie des paramètres git
 if [ -f $HOME/.gitconfig ]; then
     mv $HOME/.gitconfig $HOME/.gitconfig.original
 fi
-cp -f $HOME/dotfiles/.gitconfig $HOME/
+cp -f ./.gitconfig $HOME/
